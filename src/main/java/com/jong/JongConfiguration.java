4a89d3b4be120b12322b0c2054d20b2ff08590f1
JongHae.Park <jong.park@linecorp.com>
2025-06-29 16:40:35 +0900

    profile

--- src/main/java/com/jong/JongConfiguration.java
+++ src/main/java/com/jong/JongConfiguration.java
@@ -2,6 +2,7 @@ package com.jong;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.jong.service.JongService;
 
@@ -9,8 +10,15 @@ import com.jong.service.JongService;
 public class JongConfiguration {
 
 
+    @Profile("!jong")
     @Bean("configuredJongService")
     public JongService jongService() {
+        return new JongService();
+    }
+
+    @Profile("jong")
+    @Bean("configuredJongServiceWithProfile")
+    public JongService jongServiceWithProfile() {
         return new JongService();
     }
 }
